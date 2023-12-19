2b3f792636e64c1ed15aabe2b43c368ed619830d
Wojtek <plonkawojtek57@gmail.com>
2023-12-19 14:47:46 +0100

    frst commit on webstom

---
+++ 19.12/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+
+
+
+
+function App() {
+  return (
+      <BrowserRouter>
+         <Routes>
+           <Route path='/' element={<Homepage/>}/>
+           <Route path='/about' element={<About/>}/>
+         </Routes>
+      </BrowserRouter>
+  )
+  }
+
+export default App
